4542fb60f950296e61ca1d10a41cb10dd55104c4
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-15 20:23:51 -0500

    Initial Commit

---
+++ LazyDemons/LazyDemonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemonsApp.swift
+//  LazyDemons
+//
+//  Created by Nick Nedjat on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
